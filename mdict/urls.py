54003551e83a643258cad0a96b9ad8cc98914c78
jiang <jiangnianshun@outlook.com>
2021-05-01 20:50:57 +0800

    fix trivial

--- mdict/urls.py
+++ mdict/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^(\d+)/(.+)/', views.search_mdd),
     url(r'^dic/(\d+)/(.+)/', views.search_mdd),
     url(r'^es/(\d+)/(.+)/', views.search_mdd),
+    url(r'^esdic/(\d+)/(.+)/', views.search_mdd),
     path('exfile/', views.get_external_file),
     url('exfile/(.+)', views.get_external_file),
     path('es/exfile/', views.get_external_file),
