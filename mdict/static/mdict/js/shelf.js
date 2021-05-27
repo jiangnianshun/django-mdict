14e92feb9a65ce8f14e5a25c7e3a210c94d266e7
jiang <jiangnianshun@outlook.com>
2021-05-27 21:25:11 +0800

    add shelf 2

--- mdict/static/mdict/js/shelf.js
+++ mdict/static/mdict/js/shelf.js
@@ -32,7 +32,7 @@ function get_items(container){
                     <div class="card shadow" title="${html_escape(dic_name)}" data-pk=${dic_pk} data-name="${html_escape(dic_name)}" onclick="start_modal(this)">
                         <div class="card-body text-center">
                             <img class="" src="${html_escape(dic_icon,false)}"></img>
-                            <div class="card-title"><b class="text-primary">${dic_pror}</b>&nbsp;${html_escape(dic_name)}</div>
+                            <div class="card-title" data-file=${html_escape(dic_file)}><b class="text-primary">${dic_pror}</b>&nbsp;${html_escape(dic_name)}</div>
                         </div>
                     </div>
                 </div>
@@ -40,6 +40,7 @@ function get_items(container){
 
 				container.append(s);
 			}
+			set_dic_num();
 		},
 		error:function(jqXHR,textStatus,errorThrown){
 			alert(jqXHR.responseText);
@@ -142,4 +143,102 @@ function shelf_dic_btn(type){
     }else{
         window.open('/admin/mdict/mdictdic/'+dic_pk+'/');
     }
+}
+
+function set_dic_num(){
+    $("#dic-num").text($(".card,.shadow:visible").length);
+}
+
+function start_dic_filter(group_pk){
+    data={"dic_group":group_pk};
+    $.ajax({
+		url:"/mdict/getpkingroup/",
+		contentType:'json',
+		type:'GET',
+		data:data,
+		success:function(data){
+		var pk_list=$.parseJSON(data);
+		var mdict_list=$(".col");
+		if(pk_list.length==0&&group_pk<=0){
+		    $(mdict_list).each(function(){$(this).show();});
+		    set_dic_num()
+		}else{
+            for(var i=0;i<mdict_list.length;i++){
+                var dic_pk=parseInt($(mdict_list[i]).children('.card').attr('data-pk'));
+                var pk_pos=$.inArray(dic_pk,pk_list);
+                if(pk_pos<0){
+                    $(mdict_list[i]).hide();
+                }else{
+                    $(mdict_list[i]).show();
+                }
+            }
+		    set_dic_num()
+        }
+
+		},
+		error:function(jqXHR,textStatus,errorThrown){
+			alert(jqXHR.responseText);
+		},
+	});
+}
+
+function init_dropdown(){
+	$.ajax({
+		url:"/mdict/dicgroup/",
+		contentType:'json',
+		type:'GET',
+		async:false,
+		success:function(data){
+		var dic_group=$.parseJSON(data);
+		for(var i=0;i<dic_group.length;i++){
+			var ele='<li onclick="start_dic_filter('+dic_group[i][0]+')"><span class="dropdown-item" data-pk='+dic_group[i][0]+'>'+dic_group[i][1]+'</span></li>'
+			$('#dic-group').append($(ele));
+		}
+		},
+		error:function(jqXHR,textStatus,errorThrown){
+			alert(jqXHR.responseText);
+		},
+	});
+}
+
+function init_mdict_filter(){
+    $("#mdict-filter-input").bind("input propertychange",function(event){
+        //juery的change事件，只有当input没有聚焦的时候才能触发，input propertychange能检测input输入过程中的变化
+        var txt=$.trim($(this).val().toLowerCase( ));
+        var mdict_list=$(".col");
+        if(txt.length>0){
+//延时有问题
+            last =  event.timeStamp;
+            //利用event的timeStamp来标记时间，这样每次事件都会修改last的值，注意last必需为全局变量
+            setTimeout(function(){    //设时延迟0.5s执行
+                if(last-event.timeStamp==0)
+                //如果时间差为0（也就是你停止输入0.5s之内都没有其它的keyup事件发生）则做你想要做的事
+                {
+
+                    for(var i=0;i<=mdict_list.length;i++){
+                        var dic_a=$(mdict_list[i]).find('.card-title');
+                        var title=dic_a.text().toLowerCase();
+                        if(title=="")continue;
+                        var file=dic_a.attr('data-file').toLowerCase();
+
+                        if(t2s(title).indexOf(txt)==-1&&s2t(title).indexOf(txt)==-1&&t2s(file).indexOf(txt)==-1&&s2t(file).indexOf(txt)==-1){
+                            $(mdict_list[i]).hide();
+                        }else{
+                            $(mdict_list[i]).show();
+                        }
+                    }
+                }
+                set_dic_num();
+            },500);
+        }else{
+           last =  event.timeStamp;
+           setTimeout(function(){//这里也要定时，否则在手机上，这里运行快，上面延时，快速删除时，显示结果不对
+               for(var i=0;i<=mdict_list.length;i++){
+                    $(mdict_list[i]).show();
+               }
+               set_dic_num();
+           },500);
+        }
+
+    })
 }
