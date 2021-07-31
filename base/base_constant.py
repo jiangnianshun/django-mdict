a5f3289b8f147454137fefc0b8d33efcac722878
jiang <jiangnianshun@outlook.com>
2021-07-31 17:46:47 +0800

    trivial

--- base/base_constant.py
+++ base/base_constant.py
@@ -6,6 +6,9 @@ regp = re.compile(reg)
 builtin_dic_prefix = '''
 <link rel='stylesheet' href='/static/bootstrap/css/bootstrap.min.css'>
 <link rel='stylesheet' href='/static/mdict/css/mdict.css'>
+<style>
+    table,th,td{border-width:1px !important;border-collapse:separate !important;}
+</style>
 <script>
     MathJax = {
       tex: {
