0bcb4848208761105c30e222ad9f911e1d6e9e70
jiang <jiangnianshun@outlook.com>
2021-08-05 06:01:37 +0800

    trivial

--- mdict/static/mdict/ckeditor/plugins/mruby/plugin.js
+++ mdict/static/mdict/ckeditor/plugins/mruby/plugin.js
@@ -10,16 +10,24 @@
                 item_group.forEach(function(item){
                     if(item!=""){
                         var lmark = item.indexOf('[');
+                        var ltype = 1;
                         if(lmark<0){
                             lmark = item.indexOf('「');
+                            ltype = 2;
                         }
                         if(lmark<0){
                             html_str+=item;
+                            if(ltype==1){html_str+="]"}else{html_str+="」"}
                         }else{
                             var kanji=item.substring(0,lmark);
                             var kana=item.substring(lmark+1);
-                            var rubyText="<ruby><rb>"+kanji+"</rb><rt>"+kana+"</rt><rp>("+kana+")</rp></ruby>";
-                            html_str+=rubyText;
+                            if(kanji==""||kana==""||kanji==" "||kana==" "){
+                                html_str+=item;
+                                if(ltype==1){html_str+="]"}else{html_str+="」"}
+                            }else{
+                                var rubyText="<ruby><rb>"+kanji+"</rb><rt>"+kana+"</rt><rp>("+kana+")</rp></ruby>";
+                                html_str+=rubyText;
+                            }
                         }
                     }
                 })
