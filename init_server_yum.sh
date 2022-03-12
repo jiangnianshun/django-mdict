4084c3440f8dc3bed48e977380a5009c88d886b5
jiangnianshun <jiangnianshun@outlook.com>
2022-03-12 23:25:07 +0800

    add yum script 1

---
+++ init_server_yum.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+cd `dirname  $0`
+echo "installing dependencies..."
+yum install -y python3 python3-devel python3-pip epel-release lzo-devel lzo-minilzo zlib-devel libjpeg-turbo-devel
+pip3 install -r requirements1.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install -r requirements2.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+file="db.sqlite3"
+if [ ! -f $file ]
+then
+echo "initializing db.sqlite3..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py makemigrations mdict
+python3 manage.py migrate mdict
+python3 manage.py makemigrations mynav
+python3 manage.py migrate mynav
+python3 manage.py createsuperuser
+else
+echo "db.sqlite3 already exists..."
+fi
+chmod 777 db.sqlite3
+cd mdict/readlib/pyx
+echo "cython compiling..."
+source build.sh
+cd ../../../
---
+++ run_server_apt.sh
@@ -0,0 +1,14 @@
+file="mdict_path.json"
+if [ ! -f $file ]; then
+echo "set mdict path and audio path..."
+read -p "input mdict path: " mdict_path
+if [ $mdict_path ]; then
+python3 init_mdict_path.py -m $mdict_path
+fi
+read -p "input audio path: " audio_path
+if [ $audio_path ]; then
+python3 init_mdict_path.py -a $audio_path
+fi
+fi
+source init_server_apt.sh
+python3 manage.py runserver 0.0.0.0:8000 --noreload
